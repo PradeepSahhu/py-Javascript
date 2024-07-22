6ed60b061f4cea03f3d29e89ad28ae836e9724c3
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-07-22 21:29:13 +0530

    Server to frontend and graph on frontend

---
+++ app.py
@@ -0,0 +1,45 @@
+from os import sendfile
+from flask import Flask, jsonify, render_template
+from flask import send_file
+import matplotlib
+matplotlib.use('Agg')  # Use a non-GUI backend
+import matplotlib.pyplot as plt
+import io
+import base64
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/multiplication_table/<int:number>')
+def multiplication_table(number):
+    table = {i: i * number for i in range(1, 11)}
+    return jsonify(table)
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3], [4, 5, 6])
+    ax.set_title('Sample Plot')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+
+    # Save the plot to a BytesIO object
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    return send_file(img, mimetype='image/png', as_attachment=False)
+
+@app.route("/pradeep")
+def greetings():
+    message = {"name":"Pradeep Sahu","uid":"22BCS10284"}
+    return jsonify(message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
